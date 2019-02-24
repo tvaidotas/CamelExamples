ce6450c042b243799d025dd7ac8a6305a3322884
tvaidotas <tadas.vaidotas@gmail.com>
2019-02-24 22:28:14 +0000

    another example

---
+++ src/main/java/camel/ftptojmswithpredicate/FtpToJMSWithPredicate.java
@@ -0,0 +1,42 @@
+package camel.ftptojmswithpredicate;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+import javax.jms.ConnectionFactory;
+
+public class FtpToJMSWithPredicate {
+
+    public static void main(String args[]) throws Exception {
+        CamelContext context = new DefaultCamelContext();
+
+        // connect to embedded ActiveMQ JMS broker
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
+        // add the broker as a component
+        context.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+
+        // make sure activemq is started ./activemq start
+        // add our route to the CamelContext
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() {
+                // connect to FTP server
+                // copy all the files in root directory and put them as messages onto the queue
+                from("ftp://test.rebex.net?username=demo&password=password")
+                    .choice()
+                        .when(header("CamelFileName").endsWith(".xml"))
+                            .to("activemq:queue:xmlorders")
+                        .when(header("CamelFileName").endsWith(".csv"))
+                            .to("activemq:queue:csvorders");
+            }
+        });
+
+        context.start();
+        Thread.sleep(10000); // need sleep to keep JVM running until the job is done
+        context.stop();
+    }
+
+}
