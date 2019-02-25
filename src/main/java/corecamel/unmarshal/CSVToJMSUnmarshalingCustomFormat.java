50d6815603b515f0f80115bd23a9aad576229659
tvaidotas <tadas.vaidotas@gmail.com>
2019-02-25 19:11:54 +0000

    more examples

--- src/main/java/corecamel/unmarshal/CSVToJMSUnmarshalingCustomFormat.java
+++ src/main/java/corecamel/unmarshal/CSVToJMSUnmarshalingCustomFormat.java
@@ -2,8 +2,6 @@ package corecamel.unmarshal;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
-import org.apache.camel.Exchange;
-import org.apache.camel.Expression;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.dataformat.csv.CsvDataFormat;
@@ -11,7 +9,7 @@ import org.apache.camel.impl.DefaultCamelContext;
 
 import javax.jms.ConnectionFactory;
 
-public class CSVToJMSUnmarshaling {
+public class CSVToJMSUnmarshalingCustomFormat {
 
     public static void main(String args[]) throws Exception {
         CamelContext context = new DefaultCamelContext();
@@ -22,14 +20,15 @@ public class CSVToJMSUnmarshaling {
         context.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
 
         context.addRoutes(new RouteBuilder() {
-            public void configure() {
-            from("file:inboxcsv?noop=true&fileName=employees.csv")
-                .unmarshal().csv()
-                .split(body())
-                .to("activemq:queue:csvrecord");
-            }
-          }
-        );
+              public void configure() {
+                  CsvDataFormat myCsv = new CsvDataFormat()
+                  .setDelimiter(',') // values separated by comma
+                  .setHeader(new String[]{"id", "name", "amount"});
+                  from("file:inboxcsv?noop=true&fileName=employees.csv")
+                  .marshal(myCsv)
+                  .to("activemq:queue:csvrecord");
+              }
+        });
         context.start();
         Thread.sleep(10000); // need sleep to keep JVM running until the job is done
         context.stop();
